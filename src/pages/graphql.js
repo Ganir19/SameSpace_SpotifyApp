439b3bea2f590d1c34ef9b80e42e807f054b49cb
Tammineni Ganesh <138612129+Ganir19@users.noreply.github.com>
2024-08-19 06:23:02 +0530

    Add files via upload

---
+++ src/pages/graphql.js
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client';
+export const GET_SONGS = gql`
+  query songs{
+    songs{
+    id
+    artist
+    url
+    accent
+    name
+    top_track
+    cover{
+        id
+    }
+    }
+}
+`;
